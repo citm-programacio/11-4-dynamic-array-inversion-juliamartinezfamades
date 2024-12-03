955855a31d4d77aab7add6f517ba8617281d9eb7
Júlia Martínez Famades <julia.martinez.famades@estudiantat.upc.edu>
2024-12-03 16:14:57 +0100

    invertido completo

--- dynamicarray2.cpp
+++ dynamicarray2.cpp
@@ -1,19 +1,105 @@
-// dynamicarray2.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
+#include <cassert>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class DynamicArray {
+private:
+    int size = 0;
+    int capacity;
+    string* array;
+
+public:
+    DynamicArray() {
+        capacity = 10;
+        array = new string[capacity];
+    }
+
+    DynamicArray(int _c) {
+        capacity = _c;
+        array = new string[capacity];
+    }
+
+    DynamicArray(int _c, string* init, int init_size) {
+        capacity = _c;
+        size = init_size;
+        array = new string[capacity];
+        for (int i = 0; i < init_size; i++) {
+            array[i] = init[i];
+        }
+    }
+
+    bool insert(string newElement, int position) {
+        if (position < 0 || position > size) {
+            cout << "Posicion invalida" << endl;
+            return false;
+        }
+
+        if (capacity <= size) {
+            capacity *= 2;
+        }
+
+        string* newArray = new string[capacity];
+        size++;
+        int i;
+        for (i = 0; i < position; i++) {
+            newArray[i] = array[i];
+        }
+
+        newArray[i] = newElement;
+        i++;
+        while (i < size) {
+            newArray[i] = array[i - 1];
+            i++;
+        }
+        delete[] array;
+        array = newArray;
+        return true;
+    }
+
+    bool remove(int position) {
+        if (position >= size || position < 0) {
+            cout << "Posicion inexistente" << endl;
+            return false;
+        }
+        string* newArray = new string[capacity];
+        size--;
+        for (int i = 0; i < position; i++) {
+            newArray[i] = array[i];
+        }
+        for (int i = position; i < size; i++) {
+            newArray[i] = array[i + 1];
+        }
+        delete[] array;
+        array = newArray;
+        return true;
+    }
+
+    void print() {
+        for (int i = 0; i < size; i++) {
+            cout << array[i] << ", ";
+        }
+        cout << endl;
+    }
+
+    void invert() {
+        for (int i = 0; i < size / 2; i++) {
+            swap(array[i], array[size - i - 1]);
+        }
+    }
+};
+
+int main() {
+    DynamicArray a(10);
+    a.insert("bli", 0);
+    a.insert("blo", 0);
+    a.insert("blu", 0);
+
+    cout << "Array original:" << endl;
+    a.print();
+
+    a.invert();
+    cout << "Array invertida:" << endl;
+    a.print();
+
+    return 0;
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
